de080ea57dd9a3053855fd0a615e16546ea487e3
pgl <1801688332@qq.com>
2022-02-14 22:18:23 +0800

    fix: change

--- src/loadsh_/hooks/useState.js
+++ src/loadsh_/hooks/useState.js
@@ -2,14 +2,14 @@
 // 手动实现hooks中的useState
 
 // 定义初始值
-let currenInitValue;
+let currenValue;
 // 单次调用
-function _UseState(initValue = null) {
+function _UseState(initialValue = null) {
   // 一个是用户输入值/默认初始值
-  const state = currenInitValue || initValue;
+  const state = currenValue || initialValue;
 
   const newState = newValue => {
-    currenInitValue = newValue;
+    currenValue = newValue;
     // 进行render树更新 类似于setState
     render();
   }
@@ -21,16 +21,16 @@ function _UseState(initValue = null) {
 
 // 多次调用
 let index = 0;
-let currentValueBox = [];
+let currenInitValueBox = [];
 function _UseStates(initvalue) {
   index++;
   const currentIndex = index;
-  currenInitValue[currentIndex] = currenInitValue[currentIndex] || initvalue;
-  const newValue = newValue => {
-    currenInitValue[currentIndex] = newValue;
+  currenInitValueBox[currentIndex] = currenInitValueBox[currentIndex] || initvalue;
+  const setValue = newValue => {
+    currenInitValueBox[currentIndex] = newValue;
     render();
   }
-  return [currenInitValue[currentIndex], newValue];
+  return [currenInitValueBox[currentIndex], setValue];
 }
 
 const [init, setInit] = _UseState(0);
