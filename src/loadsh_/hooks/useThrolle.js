f414a132049a2ef4bce44657a7df07c127e41ac7
彭格列 <1801688332@qq.com>
2022-02-18 18:09:47 +0800

    feat: useThrolle

---
+++ src/loadsh_/hooks/useThrolle.js
@@ -0,0 +1,24 @@
+
+// hook
+useThrottle = (fn, delay, dep = []) => {
+  const { current } = React.useRef({ fn, timer: null });
+  React.useEffect(
+    function () {
+      current.fn = fn;
+    },
+    [fn]
+  );
+
+  return React.useCallback(function f(...arg) {
+    // if (!current.timer) {
+    //   current.timer = setTimeout(() => {
+    //     delete current.timer;
+    //   }, delay);
+    //   current.fn.call(this, ...arg);
+    // }
+    delete current.timer;
+    current.timer = setTimeout(() => {
+      current.fn.call(this, ...arg);
+    }, delay);
+  }, dep);
+};
