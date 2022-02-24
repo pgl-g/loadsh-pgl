7fbb2eda138fd22adc4165d31236900468513528
彭格列 <1801688332@qq.com>
2022-02-24 17:43:28 +0800

    fix: merge

---
+++ src/loadsh_/bind.js
@@ -0,0 +1,17 @@
+
+
+function f(b) {
+  console.log(this.a, b);
+}
+
+//=> 3, 4
+f.fakeBind({ a: 3 })(4);
+
+//=> 3, 10
+f.fakeBind({ a: 3 }, 10)(11);
+
+Function.prototype.fakeBind = function(obj, ...args) {
+  console.log(obj, ...args);
+  return (...rest) => this.call(obj, ...args, ...rest);
+}
+
