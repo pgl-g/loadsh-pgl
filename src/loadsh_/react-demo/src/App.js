d8b626c5e8c20978cf181a641853768715cc6abc
彭格列 <1801688332@qq.com>
2022-03-01 16:12:57 +0800

    feat: useReducer使用

--- src/loadsh_/react-demo/src/App.js
+++ src/loadsh_/react-demo/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 // import _UseEffect from './pages/_useEffect';
-import _UseCallback from './pages/_useCallback';
+// import _UseCallback from './pages/_useCallback';
+import _UseReducer from './pages/_useReducer';
 
 function App() {
   return (
     <div className="App">
-      <_UseCallback />
+      <_UseReducer />
     </div>
   );
 }
