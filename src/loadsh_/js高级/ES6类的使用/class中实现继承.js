746a84c41ce1140550f5921890e46dd48eebbb38
pgl <1801688332@qq.com>
2022-03-01 23:16:32 +0800

    feat: add

--- src/loadsh_/js高级/ES6类的使用/class中实现继承.js
+++ src/loadsh_/js高级/ES6类的使用/class中实现继承.js
@@ -16,6 +16,10 @@ class Person {
   personMethon() {
     console.log('父类的方法');
   }
+
+  static personStic() {
+    console.log('父类的静态方法');
+  }
 }
 
 // 继承
@@ -36,6 +40,12 @@ class Student extends Person {
   personMethon() {
     console.log('重写父类的方法');
   }
+
+  static personStic() {
+    super.personStic();
+
+    console.log('重写父类的静态方法');
+  }
   
 
 
@@ -46,5 +56,9 @@ let stu = new Student('pgl', 18);
 // 子类也可继承父类的方法
 stu.running();
 stu.personMethon();
+
+
+// 直接通过类来进行访问静态方法
+Student.personStic();
 console.log(stu)
 // console.log(Object.getOwnPropertyDescriptors(stu.__proto__.__proto__))
