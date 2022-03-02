9dd51e9d745da7479e22466f6664e5982f7158e7
彭格列 <1801688332@qq.com>
2022-03-02 16:10:05 +0800

    fix: merge

---
+++ src/loadsh_/js高级/ES6类的使用/ES6转ES5的代码.js
@@ -0,0 +1,18 @@
+
+/**
+ * es6转es5  babel => 将语法转换成浏览器能够识别 的语法
+ */
+class Person {
+
+}
+
+// babel
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof(Constructor))) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+var Person = function Person() {};
+
+Person();
