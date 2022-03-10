3c752b50fe4b1ce2ae418f5fed1e6333830fdbf6
彭格列 <1801688332@qq.com>
2022-03-10 15:21:38 +0800

    fix: change

--- src/loadsh_/手写56js/洗牌算法.js
+++ src/loadsh_/手写56js/洗牌算法.js
@@ -1,24 +1,13 @@
-
-
 let arr = [1, 2, 3];
-
 
 function filte(arr) {
   let result = [];
-
   while(arr.length > 0) {
-    
-
     let num = Math.floor(Math.random() * arr.length);
-
     result.push(arr.splice(num, 1)[0]);
-
-
   }
-
   return result
 }
-
 let res = filte(arr);
-
 console.log(res)
+
