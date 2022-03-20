0c4d48327fb8ad1055ee2c6290e98d1a3c0f3e1d
彭格列 <1801688332@qq.com>
2022-03-20 16:13:44 +0800

    fix: changw

---
+++ src/loadsh_/算法/字符串最后一个单词的长度.js
@@ -0,0 +1,33 @@
+
+// 输入 hello nowcoder
+// 输出 nowcoder 8
+
+// let str = 'hello nowcoder';
+
+// strLastLen(str);
+
+// // 转数组
+// function strLastLen(str) {
+//   let arr = str.split(' ');
+//   return arr[arr.length - 1].length;
+// }
+// console.log(strLastLen(str))
+
+// let str = 'ABCabc';
+
+// strFn(str, 'A');
+
+// function strFn(str, n) {
+//   // console.log(str, n)
+//   let arr = str.split('');
+//   // console.log(arr);
+//   let index = 0;
+//   arr.forEach(item => {
+//     if (item.toUpperCase() === n) {
+//       index++
+//     }
+//   })
+//   return index;
+// }
+
+
