fb6ac910ef635b96ef8030ef186399b02ee52ac7
彭格列 <1801688332@qq.com>
2022-03-22 22:26:38 +0800

    fix: 闭

--- src/loadsh_/js高级/闭包的定义/闭包的使用.js
+++ src/loadsh_/js高级/闭包的定义/闭包的使用.js
@@ -1,17 +1,42 @@
 
-function foo() {
-  console.log('foo');
-}
+// function foo() {
+//   console.log('foo');
+// }
 
 
-// 返回新的数组
-Array.prototype.filter = function(fn) {
-  let result = [];
-  for (let i = 0; i < this.length; i++) {
-    if (fn(this[i], i, this)) {
-      result.push(this[i]);
-    }
+// // 返回新的数组
+// Array.prototype.filter = function(fn) {
+//   let result = [];
+//   for (let i = 0; i < this.length; i++) {
+//     if (fn(this[i], i, this)) {
+//       result.push(this[i]);
+//     }
+//   }
+
+//   return result;
+// }
+
+function testArray() {
+
+  var arr = new Array(1024 * 1024).fill(1);
+  return function() {
+    console.log(arr.length);
   }
 
-  return result;
 }
+
+var arrFns = [];
+for (let i = 0; i < 100; i++) {
+  setTimeout(() => {
+    arrFns.push(testArray());
+  }, 100 * i);
+}
+
+// arrFns = null
+setTimeout(() => {
+  for (let i = 0; i < 50; i++) {
+    setTimeout(() => {
+      arrFns.pop();
+    }, 100 * 1)
+  }
+}, 10000);
