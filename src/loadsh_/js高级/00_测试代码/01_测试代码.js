47c77f65296dcb81c204d47fb10a0ea6430484cc
彭格列 <1801688332@qq.com>
2022-03-27 14:49:45 +0800

    fix: demo apply

--- src/loadsh_/js高级/00_测试代码/01_测试代码.js
+++ src/loadsh_/js高级/00_测试代码/01_测试代码.js
@@ -10,31 +10,59 @@
 // var nums = [1,2,3]
 // nums.forEach(foo,obj)
 
-Function.prototype.pgCall = function(thisArg) {
+// Function.prototype.pgCall = function(thisArg) {
 
-  // this传进来的是foo函数/隐式调用
-  var fn = this;
-  thisArg = thisArg ? Object(thisArg) : window;
+//   // this传进来的是foo函数/隐式调用
+//   var fn = this;
+//   thisArg = thisArg ? Object(thisArg) : window;
+//   thisArg.fn = fn;
+//   thisArg.fn();
+// }
+
+// function foo() {
+//   console.log('函数执行了', this);
+// }
+
+
+// function add(num1, num2) {
+//   var num3 = 1;
+//   console.log(this, num1, num2);
+//   return num1 + num2;
+// }
+
+// var obj = {
+//   num1: 1,
+// }
+
+
+// const result = add.call(obj, 20, 20);
+// console.log(result);
+// foo.pgCall({name: '1'});
+
+// 封装apply
+
+Function.prototype.pgApply = function(thisArg, argArray) {
+
+  let fn = this;
+
   thisArg.fn = fn;
-  thisArg.fn();
+
+  argArray = argArray ? argArray : [];
+
+  const result = thisArg.fn(...argArray);
+
+  delete thisArg.fn;
+
+  return result
 }
+
 
 function foo() {
-  console.log('函数执行了', this);
+  console.log(this, 'foo被调用了');
 }
-
 
 function add(num1, num2) {
-  var num3 = 1;
   console.log(this, num1, num2);
-  return num1 + num2;
 }
-
-var obj = {
-  num1: 1,
-}
-
-
-const result = add.call(obj, 20, 20);
-console.log(result);
-foo.pgCall({name: '1'});
+add.pgApply({}, [12, 2]);
+// add.apply(this, [1, 2]);
