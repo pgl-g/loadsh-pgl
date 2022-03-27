0a4a9ccf470c5027e144e9082a41b15b73605f70
彭格列 <1801688332@qq.com>
2022-03-27 14:32:15 +0800

    fix: change

--- src/loadsh_/js高级/00_测试代码/01_测试代码.js
+++ src/loadsh_/js高级/00_测试代码/01_测试代码.js
@@ -1,11 +1,40 @@
 // 你不知道的JavaScript
-function foo(el) {
-  console.log(el,this.id)
+// function foo(el) {
+//   console.log(el,this.id)
+// }
+
+// var obj = {
+//   id:'awesome'
+// }
+
+// var nums = [1,2,3]
+// nums.forEach(foo,obj)
+
+Function.prototype.pgCall = function(thisArg) {
+
+  // this传进来的是foo函数/隐式调用
+  var fn = this;
+  thisArg = thisArg ? Object(thisArg) : window;
+  thisArg.fn = fn;
+  thisArg.fn();
+}
+
+function foo() {
+  console.log('函数执行了', this);
+}
+
+
+function add(num1, num2) {
+  var num3 = 1;
+  console.log(this, num1, num2);
+  return num1 + num2;
 }
 
 var obj = {
-  id:'awesome'
+  num1: 1,
 }
 
-var nums = [1,2,3]
-nums.forEach(foo,obj)
+
+const result = add.call(obj, 20, 20);
+console.log(result);
+foo.pgCall({name: '1'});
