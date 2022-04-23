a0bbb59416df2454d9814bc06a3d000e857c8bcb
彭格列 <1801688332@qq.com>
2022-04-23 15:32:57 +0800

    fix: 了解reflect的作用及api

--- src/loadsh_/手写56js/vue2-3响应式原理.js
+++ src/loadsh_/手写56js/vue2-3响应式原理.js
@@ -64,23 +64,78 @@
 
 
 
-let obj = {
-  name: 'pgl',
-  age: 18
+// let obj = {
+//   name: 'pgl',
+//   age: 18,
+//   get: function () {
+//     return this.name;
+//   },
+//   set: function(newVal) {
+//     this.name = newVal
+//   }
+// }
+
+
+// const newProxy = new Proxy(obj, {
+//   get(target, key, receiver) {
+//     console.log(Reflect.get(target, key), '------', receiver)
+//     return Reflect.get(target, key);
+//   },
+//   set(target, key, newVal, receiver) {
+//     // target[key] = newVal
+//     Reflect.set(target, key, newVal, receiver);
+//   }
+// })
+ 
+// newProxy.name = 'xx'
+
+// console.log(obj)
+// console.log(obj, newProxy)
+
+
+// const obj = {
+//   _name: "why",
+//   get name() {
+//     return this._name
+//   },
+//   set name(newValue) {
+//     this._name = newValue
+//   }
+// }
+
+// const objProxy = new Proxy(obj, {
+//   get: function(target, key, receiver) {
+//     // receiver是创建出来的代理对象
+//     console.log("get方法被访问--------", key, receiver)
+//     console.log(receiver === objProxy)
+//     return Reflect.get(target, key, receiver)
+//   },
+//   set: function(target, key, newValue, receiver) {
+//     console.log("set方法被访问--------", key)
+//     Reflect.set(target, key, newValue, receiver)
+//   }
+// })
+
+// // objProxy.name = "kobe"
+// console.log(objProxy)
+// console.log(objProxy.name)
+
+
+function Student(name, age) {
+
+  this.name = name;
+  this.age = age;
+  
 }
 
 
-const newProxy = new Proxy(obj, {
-  get(target, key, receiver) {
-    console.log(Reflect.get(target, key), '------')
-    return Reflect.get(target, key);
-  },
-  set(target, key, newVal, receiver) {
-    // target[key] = newVal
-    Reflect.set(target, key, newVal, receiver);
-  }
-})
- 
-newProxy.name = 'xx'
+function Teacher() {
+  console.log(this, '0---')
+}
 
-console.log(obj, newProxy)
+const obj = Reflect.construct(Student, ['张三', 12], Teacher);
+
+
+console.log(obj)
+
+
